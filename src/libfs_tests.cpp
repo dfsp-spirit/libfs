aa1c87e8f18583b072312fb63c1756b5db742741
Tim Schäfer <ts+code@rcmd.org>
2021-07-07 19:43:21 +0200

    add more curv tests

--- src/libfs_tests.cpp
+++ src/libfs_tests.cpp
@@ -2,16 +2,38 @@
 #include "libfs.h"
 #include "catch.hpp"
 #include <vector>
+#include <algorithm>
+#include <iterator>
 
-TEST_CASE( "test function" ) {
 
-    SECTION( "twotimes with float input" ) {
+TEST_CASE( "The sample twotimes function works" ) {
+
+    SECTION( "The twotimes works with float input" ) {
         float res = fs::twotimes(5.0);
         REQUIRE( res >= 9.0);
     }
 
-    SECTION("Reading curv file works" ) {
-        std::vector<float> data = fs::read_curv("examples/read_curv/lh.thickness");
+}
+
+TEST_CASE( "Reading the demo curv file works" ) {
+
+    std::vector<float> data = fs::read_curv("examples/read_curv/lh.thickness");
+
+    SECTION("The number of values read is correct" ) {        
         REQUIRE( data.size() == 149244);
     }
+
+    SECTION("The range of the values read is correct" ) {    
+        float min_entry = *std::min_element(data.begin(), data.end());
+        float max_entry = *std::max_element(data.begin(), data.end());    
+        REQUIRE(min_entry == Approx(0.0));
+        REQUIRE(max_entry == Approx(5.0));
+    }
+
+    SECTION("Some values are as expected") {
+        REQUIRE(data[0] == Approx(2.561705));
+        REQUIRE(data[100] == Approx(2.579938));
+        REQUIRE(data[100000] == Approx(0.0));
+    }
+
 }
