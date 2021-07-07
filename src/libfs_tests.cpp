4bbf4755670f06ee58ac3958702d9f39ea36f304
Tim Schäfer <ts+code@rcmd.org>
2021-07-07 18:40:45 +0200

    add unit test for read_curv

--- src/libfs_tests.cpp
+++ src/libfs_tests.cpp
@@ -1,11 +1,17 @@
 
 #include "libfs.h"
 #include "catch.hpp"
+#include <vector>
 
 TEST_CASE( "test function" ) {
 
     SECTION( "twotimes with float input" ) {
         float res = fs::twotimes(5.0);
         REQUIRE( res >= 9.0);
+    }
+
+    SECTION("Reading curv file works" ) {
+        std::vector<float> data = fs::read_curv("examples/read_curv/lh.thickness");
+        REQUIRE( data.size() == 149244);
     }
 }
