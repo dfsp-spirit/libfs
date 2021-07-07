c717df664498bb5c3143c5d10f05c1a6ca652a24
Tim Schäfer <ts+code@rcmd.org>
2021-07-07 16:12:14 +0200

    fix comments, add output line about data length

--- examples/read_mgh/read_mgh.cpp
+++ examples/read_mgh/read_mgh.cpp
@@ -1,5 +1,5 @@
 
-// Demo program that reads brain mesh from a surf file.
+// Demo program that reads a FreeSurfer 4D volume from an MGH file.
 // To compile this:
 // 
 //    g++ -std=c++2a -I../../include/ read_mgh.cpp -o read_mgh
@@ -20,5 +20,6 @@ int main(int argc, char** argv) {
     fs::Mgh mgh;
     fs::read_mgh(&mgh, mgh_fname);
     std::cout << "Received MGH with size " << mgh.header.dim1length << "*" << mgh.header.dim2length <<  "*" <<  mgh.header.dim3length << "*" << mgh.header.dim4length << " voxels.\n"; 
+    std::cout << "The data type is " << mgh.header.dtype << " and the length of mgh.data.data_mri_uchar is " << mgh.data.data_mri_uchar.size() << ".\n";
     exit(0);
 }
