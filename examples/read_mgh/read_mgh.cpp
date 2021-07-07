0cf71fa95f84574ea69ab966c99b8a59a0475de0
Tim Schäfer <ts+code@rcmd.org>
2021-07-07 15:59:04 +0200

    add example code and example MGH file

---
+++ examples/read_mgh/read_mgh.cpp
@@ -0,0 +1,24 @@
+
+// Demo program that reads brain mesh from a surf file.
+// To compile this:
+// 
+//    g++ -std=c++2a -I../../include/ read_mgh.cpp -o read_mgh
+//
+
+#include "libfs.h"
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char** argv) {
+    std::string mgh_fname = "brain.mgh";
+    if(argc == 2) {
+        mgh_fname = argv[1];
+    }
+    std::cout << "Reading input MGH file '" << mgh_fname << "'.\n";
+    fs::Mgh mgh;
+    fs::read_mgh(&mgh, mgh_fname);
+    std::cout << "Received MGH with size " << mgh.header.dim1length << "*" << mgh.header.dim2length <<  "*" <<  mgh.header.dim3length << "*" << mgh.header.dim4length << " voxels.\n"; 
+    exit(0);
+}
