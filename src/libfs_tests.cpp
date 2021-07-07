75da99127048d21bd4dc192ce4eb0a1293796979
Tim Schäfer <ts+code@rcmd.org>
2021-07-07 21:20:26 +0200

    Add MGH unittests

--- src/libfs_tests.cpp
+++ src/libfs_tests.cpp
@@ -26,5 +26,31 @@ TEST_CASE( "Reading the demo curv file works" ) {
         REQUIRE(data[100] == Approx(2.579938));
         REQUIRE(data[100000] == Approx(0.0));
     }
+}
+
+
+TEST_CASE( "Reading the demo MGH file works" ) {
+
+    fs::Mgh mgh;
+    fs8::read_mgh(&mgh, "examples/read_mgh/brain.mgh");
+
+    SECTION("The MRI_DTYPE is correct" ) {        
+        REQUIRE( mgh.header.dtype == fs::MRI_UCHAR);
+    }
+
+    SECTION("The number of values read is correct" ) {        
+        REQUIRE( mgh.data.data_mri_uchar.size() == 256*256*256);
+    }
+
+    SECTION("The range of the values read is correct" ) {    
+        uint8_t min_entry = *std::min_element(mgh.data.data_mri_uchar.begin(), mgh.data.data_mri_uchar.end());
+        uint8_t max_entry = *std::max_element(mgh.data.data_mri_uchar.begin(), mgh.data.data_mri_uchar.end());    
+        REQUIRE(min_entry == 0);
+        REQUIRE(max_entry == 128);
+    }
+
+    SECTION("Some values are as expected") {
+        REQUIRE(mgh.data.data_mri_uchar[0] == 0);
+    }
 
 }
