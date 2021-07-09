4c66d571daaf5b975694db9ac0e6588257277fcc
Tim Schäfer <ts+code@rcmd.org>
2021-07-09 12:19:09 +0200

    add WIP read_label example

---
+++ examples/read_label/read_label.cpp
@@ -0,0 +1,37 @@
+
+// Demo program that reads label data from an ASCII label file.
+// To compile this witg g++ 9.3:
+// 
+//    g++ -I../../include/ read_label.cpp -o read_label
+//
+// or with clang 10:
+//
+//    clang++ -I../../include/ read_label.cpp -o read_label
+//
+
+#include "libfs.h"
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main(int argc, char** argv) {
+    std::string label_fname = "lh.cortex.label";
+    if(argc == 2) {
+        label_fname = argv[1];
+    }
+    std::cout << "Reading input label file '" << label_fname << "'.\n";
+    fs::Label label;
+    fs::read_label(label_fname, &label);
+    float min_entry = *std::min_element(label.value.begin(), label.value.end());
+    float max_entry = *std::max_element(label.value.begin(), label.value.end());
+    if(label.value.size() > 0) {
+        std::cout << "Received " << label.value.size() << " label values in range " << min_entry  << " to " << max_entry << ".\n"; 
+    } else {
+        std::cout << "Received empty label value vector.\n";
+    }
+
+    exit(0);
+}
