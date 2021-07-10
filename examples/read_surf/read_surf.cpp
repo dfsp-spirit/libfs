0bd5a6aa38736a016f75b571f66d1b0126d0543b
Tim Schäfer <ts+code@rcmd.org>
2021-07-10 14:16:34 +0200

    export meshes to OBJ and PLY in example program

--- examples/read_surf/read_surf.cpp
+++ examples/read_surf/read_surf.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 int main(int argc, char** argv) {
     std::string surface_fname = "lh.white";
@@ -19,6 +20,23 @@ int main(int argc, char** argv) {
     std::cout << "Reading input surface file '" << surface_fname << "'.\n";
     fs::Mesh surface;
     fs::read_surf(&surface, surface_fname);
-    std::cout << "Received surface with " << surface.vertices.size() << " vertices and " << surface.faces.size()  << " faces.\n"; 
+    std::cout << "Received surface with " << surface.num_vertices() << " vertices and " << surface.num_faces()  << " faces.\n";
+
+    // Write the mesh to files in OBJ and PLY formats.
+    std::ofstream outobj("lh_white.obj");
+    if(outobj.is_open()) {
+        outobj << surface.to_obj();
+        outobj.close();
+    } else {
+        std::cerr << "Failed to open OBJ output file.\n";
+    }
+    std::ofstream outply("lh_white.ply");
+    if(outply.is_open()) {
+        outply << surface.to_ply();
+        outply.close();
+    } else {
+        std::cerr << "Failed to open PLY output file.\n";
+    }
+
     exit(0);
 }
