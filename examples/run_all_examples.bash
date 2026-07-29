b795d7c3cf9f0e89305ce90ed04a4298237e9888
Tim Schaefer <ts+code@rcmd.org>
2026-07-29 19:01:46 +0200

    NEW: pass c++11 standard in example script

--- examples/run_all_examples.bash
+++ examples/run_all_examples.bash
@@ -14,28 +14,28 @@ fi
 bd=$(pwd)
 
 echo "$apptag ========== Running annot example... =========="
-cd $bd/read_annot && g++ -I../../include/ read_annot.cpp -o read_annot && ./read_annot && cd ..
+cd $bd/read_annot && g++ -std=c++11 -I../../include/ read_annot.cpp -o read_annot && ./read_annot && cd ..
 
 echo "$apptag ========== Running curv example... =========="
-cd $bd/read_curv && g++ -I../../include/ read_curv.cpp -o read_curv && ./read_curv && cd ..
+cd $bd/read_curv && g++ -std=c++11 -I../../include/ read_curv.cpp -o read_curv && ./read_curv && cd ..
 
 echo "$apptag ========== Running label example... =========="
-cd $bd/read_label && g++ -I../../include/ read_label.cpp -o read_label && ./read_label && cd ..
+cd $bd/read_label && g++ -std=c++11 -I../../include/ read_label.cpp -o read_label && ./read_label && cd ..
 
 echo "$apptag ========== Running MGH example... =========="
-cd $bd/read_mgh && g++ -I../../include/ read_mgh.cpp -o read_mgh && ./read_mgh && cd ..
+cd $bd/read_mgh && g++ -std=c++11 -I../../include/ read_mgh.cpp -o read_mgh && ./read_mgh && cd ..
 
 echo "$apptag ========== Running MGZ example (requires zlib)... =========="
-cd $bd/read_mgz && g++ -I../../include/ -I./include_zstr/ read_mgz.cpp -o read_mgz -lz && ./read_mgz && cd ..
+cd $bd/read_mgz && g++ -std=c++11 -I../../include/ -I./include_zstr/ read_mgz.cpp -o read_mgz -lz && ./read_mgz && cd ..
 
 echo "$apptag ========== Running surf example... =========="
-cd $bd/read_surf && g++ -I../../include/ read_surf.cpp -o read_surf && ./read_surf && cd ..
+cd $bd/read_surf && g++ -std=c++11 -I../../include/ read_surf.cpp -o read_surf && ./read_surf && cd ..
 
 echo "$apptag ========== Running descriptor info example... =========="
-cd $bd/descriptor_info && g++ -I../../include/ descriptor_info.cpp -o descriptor_info && ./descriptor_info && cd ..
+cd $bd/descriptor_info && g++ -std=c++11 -I../../include/ descriptor_info.cpp -o descriptor_info && ./descriptor_info && cd ..
 
 echo "$apptag ========== Running vertex color export example... =========="
-cd $bd/vertex_color_export && g++ -I../../include/ vertex_color_export.cpp -o vertex_color_export && ./vertex_color_export && cd ..
+cd $bd/vertex_color_export && g++ -std=c++11 -I../../include/ vertex_color_export.cpp -o vertex_color_export && ./vertex_color_export && cd ..
 
 
 
