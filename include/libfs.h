5e375e783ed759948f03e1538acc4bba664ed17f
Tim Schäfer <tim.schaefer@ae.mpg.de>
2026-08-07 09:17:03 +0200

    NEW: log changes for, and bump version to v0.4.2

--- include/libfs.h
+++ include/libfs.h
@@ -17,13 +17,13 @@
 #include <cstdint>
 #include <cstring>
 
-#define LIBFS_VERSION "0.4.1"
+#define LIBFS_VERSION "0.4.2"
 #define LIBFS_VERISION_MAJOR 0   # deprecated, use LIBFS_VERSION_MAJOR instead
 #define LIBFS_VERISION_MINOR 4   # deprecated, use LIBFS_VERSION_MINOR instead
-#define LIBFS_VERISION_PATCH 1   # deprecated, use LIBFS_VERSION_PATCH instead
+#define LIBFS_VERISION_PATCH 2   # deprecated, use LIBFS_VERSION_PATCH instead
 #define LIBFS_VERSION_MAJOR 0
 #define LIBFS_VERSION_MINOR 4
-#define LIBFS_VERSION_PATCH 1
+#define LIBFS_VERSION_PATCH 2
 
 // -- Security / defensive hardening configuration -------------------------------------
 // Users can #define any of these BEFORE including libfs.h to override the defaults.
