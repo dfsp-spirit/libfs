167602cda22671784faf0de688b4b70807e29d8a
Tim Schaefer <ts+code@rcmd.org>
2026-08-08 08:52:31 +0200

    DEV: support giving single example dir name in run all script to run only 1

--- examples/run_all_examples.bash
+++ examples/run_all_examples.bash
@@ -13,32 +13,39 @@ fi
 
 bd=$(pwd)
 
-echo "$apptag ========== Running annot example... =========="
-cd $bd/read_annot && g++ -std=c++11 -I../../include/ read_annot.cpp -o read_annot && ./read_annot && cd ..
+# If a directory name is given as first argument, run only that example.
+# Otherwise run all examples.
+if [ -n "$1" ]; then
+    target="$1"
+    if [ ! -d "$bd/$target" ]; then
+        echo "$apptag ERROR: example directory '$target' not found in $bd"
+        echo "$apptag Available examples:"
+        ls -d "$bd"/*/ | while read d; do basename "$d"; done
+        exit 1
+    fi
+    echo "$apptag Running single example: $target"
+fi
 
-echo "$apptag ========== Running curv example... =========="
-cd $bd/read_curv && g++ -std=c++11 -I../../include/ read_curv.cpp -o read_curv && ./read_curv && cd ..
+run_example() {
+    local name="$1"
+    if [ -n "$target" ] && [ "$name" != "$target" ]; then
+        return 0
+    fi
+    shift
+    local cmd="$*"
+    echo "$apptag ========== Running $name example... =========="
+    (cd "$bd/$name" && eval "$cmd") || exit $?
+}
 
-echo "$apptag ========== Running label example... =========="
-cd $bd/read_label && g++ -std=c++11 -I../../include/ read_label.cpp -o read_label && ./read_label && cd ..
-
-echo "$apptag ========== Running MGH example... =========="
-cd $bd/read_mgh && g++ -std=c++11 -I../../include/ read_mgh.cpp -o read_mgh && ./read_mgh && cd ..
-
-echo "$apptag ========== Running MGZ example (zstr, requires zlib)... =========="
-cd $bd/read_mgz && g++ -std=c++11 -I../../include/ -I./include_zstr/ read_mgz.cpp -o read_mgz -lz && ./read_mgz && cd ..
-
-echo "$apptag ========== Running MGZ native example (requires zlib)... =========="
-cd $bd/read_mgz_native && g++ -std=c++11 -I../../include/ read_mgz_native.cpp -o read_mgz_native -lz && ./read_mgz_native && cd ..
-
-echo "$apptag ========== Running surf example... =========="
-cd $bd/read_surf && g++ -std=c++11 -I../../include/ read_surf.cpp -o read_surf && ./read_surf && cd ..
-
-echo "$apptag ========== Running descriptor info example... =========="
-cd $bd/descriptor_info && g++ -std=c++11 -I../../include/ descriptor_info.cpp -o descriptor_info && ./descriptor_info && cd ..
-
-echo "$apptag ========== Running vertex color export example... =========="
-cd $bd/vertex_color_export && g++ -std=c++11 -I../../include/ vertex_color_export.cpp -o vertex_color_export && ./vertex_color_export && cd ..
+run_example read_annot      "g++ -std=c++11 -I../../include/ read_annot.cpp -o read_annot && ./read_annot"
+run_example read_curv       "g++ -std=c++11 -I../../include/ read_curv.cpp -o read_curv && ./read_curv"
+run_example read_label      "g++ -std=c++11 -I../../include/ read_label.cpp -o read_label && ./read_label"
+run_example read_mgh        "g++ -std=c++11 -I../../include/ read_mgh.cpp -o read_mgh && ./read_mgh"
+run_example read_mgz        "g++ -std=c++11 -I../../include/ -I./include_zstr/ read_mgz.cpp -o read_mgz -lz && ./read_mgz"
+run_example read_mgz_native "g++ -std=c++11 -I../../include/ read_mgz_native.cpp -o read_mgz_native -lz && ./read_mgz_native"
+run_example read_surf       "g++ -std=c++11 -I../../include/ read_surf.cpp -o read_surf && ./read_surf"
+run_example descriptor_info "g++ -std=c++11 -I../../include/ descriptor_info.cpp -o descriptor_info && ./descriptor_info"
+run_example vertex_color_export "g++ -std=c++11 -I../../include/ vertex_color_export.cpp -o vertex_color_export && ./vertex_color_export"
 
 
 
